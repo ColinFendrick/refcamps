39033b342d771e970481e707b2ea14b6cfadad9b
jcalhoon21 <jcalhoon21@gmail.com>
2021-03-01 11:43:06 -0800

    Added Material-UI, set up Search and Submit Event pages, and React Router

---
+++ src/components/Buttons/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import {
+  useHistory
+} from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(2),
+      backgroundColor: 'teal',
+      color: 'white',
+      fontWeight: 700,
+      fontSize: '20px',
+      letterSpacing: '2px',
+      textTransform: 'capitalize'
+    },
+  },
+}));
+
+const Buttons = () => {
+  const classes = useStyles();
+  let history = useHistory();
+
+  const submitEvent = () => {
+    history.push("/submitEvent");
+  }
+  const searchEvent = () => {
+    history.push("/searchEvent");
+  }
+
+  return (
+    <div className={classes.root}>
+      <Button variant="contained" onClick={submitEvent}>I need referees</Button>
+      <Button variant="contained" onClick={searchEvent}>I'm looking to referee</Button>
+    </div>
+  );
+}
+
+export default Buttons;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import SearchEvent from '../src/pages/EventViews/SearchEvent';
+import SubmitEvent from '../src/pages/EventViews/SubmitEvent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/submitEvent">
+          <SubmitEvent />
+        </Route>
+        <Route path="/searchEvent">
+          <SearchEvent />
+        </Route>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
